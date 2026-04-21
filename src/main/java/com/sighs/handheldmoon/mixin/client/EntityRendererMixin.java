87c30fba616f83ade8074e83b402f0d0d441559b
Flechazo <2558755403@qq.com>
2026-04-21 20:32:06 +0800

    tiny up

--- src/main/java/com/sighs/handheldmoon/mixin/client/EntityRendererMixin.java
+++ src/main/java/com/sighs/handheldmoon/mixin/client/EntityRendererMixin.java
@@ -1,11 +1,9 @@
 package com.sighs.handheldmoon.mixin.client;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.sighs.handheldmoon.util.Utils;
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.entity.EntityRenderer;
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,11 +15,9 @@ public class EntityRendererMixin {
             method = "getPackedLightCoords",
             at = @At("RETURN")
     )
-    private int handheldMoon$forceFullLightIfUsingFlashlight(int original, Entity entity, float partialTicks) {
-        if (entity instanceof Player player) {
-            if (Utils.isUsingFlashlight(player)) {
-                return 15728880;
-            }
+    private int handheldMoon$forceBlockFullBrightIfUsingFlashlight(int original, Entity entity, float partialTicks) {
+        if (entity instanceof Player player && Utils.isUsingFlashlight(player)) {
+            return LightTexture.pack(15, LightTexture.sky(original));
         }
         return original;
     }
